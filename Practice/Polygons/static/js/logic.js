ac8f5c819662e1c601c74551555de92956ce097f
AnnaKDay <katsukoday@gmail.com>
2022-06-12 19:53:22 -0500

    starting on polygons

---
+++ Practice/Polygons/static/js/logic.js
@@ -0,0 +1,52 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satellitesStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Sattelite Streets": satellitesStreets
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+  center: [43.7, -79.3],
+  zoom: 10,
+  layers: [satellitesStreets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/AnnaKDay/Mapping-Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    L.geoJSON(data, {
+        
+        // We turn each feature into a marker on the map.
+        onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h1>" + "Airport Code: " + feature.properties.faa + "</h2>" + "<h3>"+ "Airport Name: " + feature.properties.name + "</h3>");
+        }
+    }).addTo(map);
+});
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
