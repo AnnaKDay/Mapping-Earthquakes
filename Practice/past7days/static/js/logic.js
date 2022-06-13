984bcedb002c857770d1f6754baa117d1d389583
AnnaKDay <katsukoday@gmail.com>
2022-06-12 20:53:27 -0500

    toggle data in viz

--- Practice/past7days/static/js/logic.js
+++ Practice/past7days/static/js/logic.js
@@ -22,6 +22,15 @@ let baseMaps = {
   "Satellite": satteliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
   center: [39.5, -98.5],
@@ -30,7 +39,7 @@ let map = L.map("mapid", {
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -95,7 +104,10 @@ function getColor(magnitude) {
 
       //stylized the markers
       style: styleInfo
-  }).addTo(map);
+  }).addTo(earthquakes);
+
+    //add earthquake layer to our map
+    earthquakes.addTo(map)
 });
 
 
